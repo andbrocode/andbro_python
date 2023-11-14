8bdf8887340bc88ccdf6cd8f0c464fc9f36581d2
andbrocode <andbro@tutanota.com>
2023-11-14 18:07:50 +0100

    updates andbro

---
+++ andbro__save_to_pickle.py
@@ -0,0 +1,8 @@
+def __save_to_pickle(obj, path, name):
+
+    ofile = open(path+name+".pkl", 'wb')
+    pickle.dump(obj, ofile)
+
+    if Path(path+name+".pkl").exists():
+        print(f"\n -> created:  {path}{name}.pkl")
+
